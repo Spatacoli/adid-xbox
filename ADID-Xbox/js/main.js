42da34a1b15394cdfa6798667fe55874a2e4d175
Todd Spatafore <tspatafore@fandango.com>
2024-03-29 12:02:32 -0700

    Initial push

---
+++ ADID-Xbox/js/main.js
@@ -0,0 +1,5 @@
+﻿// Your code here!
+const adid = Windows.System.UserProfile.AdvertisingManager.advertisingId;
+const content = document.getElementById("content");
+
+content.innerText = "The ADID is: " + adid;
